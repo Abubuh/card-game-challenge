f47984f598ccf7302de577da4e0998cdcb2d23e7
abubuh <cristian_caht@hotmail.com>
2026-05-18 12:22:07 -0500

    Add preventDefault for Space/Enter key on card to prevent scroll

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,17 @@ const Card = ({ image, isFlipped, onClick, type, isDisabled }) => {
   return (
     <div
       onClick={onClick}
-      onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') onClick(); }}
-      className={`w-24 h-32 md:w-20 md:h-24 lg:w-42 lg:h-54 perspective-[1000px] ${isFlipped ? "cursor-default":" cursor-pointer"}`}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          e.preventDefault();
+          onClick();
+        }
+      }}
+      className={`w-24 h-32 md:w-20 md:h-24 lg:w-42 lg:h-54 perspective-[1000px] ${isFlipped ? "cursor-default" : " cursor-pointer"}`}
       role="button"
       aria-disabled={isDisabled}
       tabIndex={0}
-      aria-label={isFlipped ? `${type} card` : 'Face down card'}
+      aria-label={isFlipped ? `${type} card` : "Face down card"}
     >
       <div
         className={`relative w-full h-full transition-transform border-black border-2 rounded-md duration-500 transform-3d ${isFlipped ? "transform-[rotateY(180deg)]" : "transform-[rotateY(0deg)]"}`}
@@ -16,9 +21,7 @@ const Card = ({ image, isFlipped, onClick, type, isDisabled }) => {
           <span className="text-yellow-400 text-6xl font-bold">?</span>
         </div>
 
-        <div
-          className="absolute inset-0 flex items-center justify-center rounded-lg bg-white transform-[rotateY(180deg)] backface-hidden"
-        >
+        <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-white transform-[rotateY(180deg)] backface-hidden">
           <img src={image} alt="" className="w-16 h-16 object-contain" />
         </div>
       </div>
